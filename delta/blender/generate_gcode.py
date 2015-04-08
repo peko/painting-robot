843b45309306824e4bdd40dfa35ac7780c392eac
peko <peko@gasubasu.com>
2015-04-08 19:54:05 +0300

    blender

--- delta/blender/generate_gcode.py
+++ delta/blender/generate_gcode.py
@@ -10,7 +10,7 @@ ml = 10 # min length
 up = 72
 
 d1 = 63 #
-d2 = 60 # 62
+d2 = 62 # 62
 dd = 0.5
 
 G = ""
@@ -21,16 +21,20 @@ zo = 0.0
 w = 1
 
 min_step = 2;
+parsed_splines = []
+splines_ends = []
+current_spline = None
 
 #cubic bezier value
 def cubic(p, t):
 	return p[0]*(1.0-t)**3.0 + 3.0*p[1]*t*(1.0-t)**2.0 + 3.0*p[2]*(t**2.0)*(1.0-t) + p[3]*t**3.0
 
+
 #length of the curve
 def seg_length(p):
 
 	length = 0.0
-	prec = 100 #precision
+	prec = 20 #precision
 	inc = 1.0/prec #increments
 	
 	#subdivide the curve in 1000 lines and sum its magnitudes
@@ -44,6 +48,7 @@ def seg_length(p):
 		length+=r
 			
 	return length
+
 
 def generate_g(x=None,y=None, z=None):
     
@@ -67,47 +72,57 @@ def generate_g(x=None,y=None, z=None):
 
 
 
-def split_spline(seg, z):
+def split_segment(seg):
+
+    global current_spline
+
+    # put firstpoint
+    current_spline.append((seg[0][0],seg[0][1],))
+
     l = seg_length(seg)
     steps = l/min_step
     steps = math.ceil(steps)
+    # if too long then  insert additional points
     if steps>0:
         for s in range(0, steps):
             v = cubic(seg,1.0/steps*s)
-            generate_g(v[0], v[1], z)
-    else:        
-        generate_g(seg[0][0], seg[0][1], z)
+            current_spline.append((v[0], v[1],))
     
 
 def parse_spline(spline):
-    
-    f = spline.bezier_points[0]
-    
-    generate_g(f.co[0], f.co[1],up)
-    
-    z = d1
-    
-    generate_g(None, None, z)
-    
-    for i in range(0, spline.point_count_u-1):
-        p = spline.bezier_points[i  ]
-        n = spline.bezier_points[i+1]
-        seg = (p.co,p.handle_right, n.handle_left,n.co,)
-        split_spline(seg, z)
 
-    if spline.use_cyclic_u :
-        p = spline.bezier_points[spline.point_count_u-1]
-        n = spline.bezier_points[0]
-        seg = (p.co,p.handle_right, n.handle_left,n.co,)
-        split_spline(seg, z)
-        # last point (first one) cyclic
-        generate_g(n.co[0], n.co[1], z)
-    else:
-        # last poin not cyclic
-        p = spline.bezier_points[spline.point_count_u-1]
-        generate_g(p.co[0], p.co[1], z)
+    global parsed_splines
+    global splines_ends
+    global current_spline 
 
-    generate_g(None, None, up)
+    current_spline = []
+    
+    # enum in [‘POLY’, ‘BEZIER’, ‘BSPLINE’, ‘CARDINAL’, ‘NURBS’], default ‘POLY’
+    if spline.type == "BEZIER": 
+        
+        f = spline.bezier_points[0]
+                
+        for i in range(0, spline.point_count_u-1):
+            p = spline.bezier_points[i  ]
+            n = spline.bezier_points[i+1]
+            seg = (p.co,p.handle_right, n.handle_left,n.co,)
+            split_segment(seg)
+
+        if not spline.use_cyclic_u :
+            # last poin not cyclic put last point
+            p = spline.bezier_points[spline.point_count_u-1]
+            current_spline.append((p.co[0], p.co[1],))
+        else:
+            p = spline.bezier_points[spline.point_count_u-1]
+            n = spline.bezier_points[0]
+            seg = (p.co,p.handle_right, n.handle_left, n.co,)
+            split_segment(seg)
+
+            # last point (first one) cyclic
+            current_spline.append((n.co[0], n.co[1],))
+
+    parsed_splines.append(current_spline)
+    splines_ends.append((spline.bezier_points,,))
 
 def MakePolyLine(objname, curvename, cList):    
     curvedata = bpy.data.curves.new(name=curvename, type='CURVE')    
@@ -128,16 +143,48 @@ def MakePolyLine(objname, curvename, cList):
     polyline.order_u = len(polyline.points)-1  
     polyline.use_endpoint_u = True  
     
-def generate_gcode(s):
-    if s.type  == "CURVE":
-        for sp in s.data.splines: 
-            parse_spline(sp)
-    return G
+
+def generate_gcode():
     
-G+="G28\n" # GO HOME        
-for s in sel:     
-    generate_gcode(s)
-G+="G28\n" # GO HOME  
+    global G
+
+    G+="G28\n" # home
+    for spline in parsed_splines:
+        l = len(spline)
+        for i in range(0, l):
+            p = spline[i]
+            if i==0:
+                generate_g(x=p[0], y=p[1],z=up) # first point UP
+            
+            generate_g(x=p[0], y=p[1], z=d2) 
+
+            if i == l-1:
+                generate_g(x=p[0], y=p[1],z=up) # UP
+    G+="G28\n"
+
+
+def reverse_spline():
+
+def optimize_spline():
+
+def merge_near_splines():
+
+def optimize_order():
+
+def parse_curve(curve):
+    for spline in curve.data.splines:
+        parse_spline(spline)
+
+    optimize_splines()
+    merge_near_splines()
+    optimize_order()
+
+for s in sel:
+    if s.type == 'CURVE':
+        parse_curve(s)
+
+
+generate_gcode()
     
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
